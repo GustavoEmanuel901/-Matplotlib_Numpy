762ed4243ecc21fd262f14c289c5cf4bd84e5c3f
Gustavo Emanuel <62678551+GustavoEmanuel901@users.noreply.github.com>
2024-09-29 22:41:22 +0000

    Aula1

--- introducao.py
+++ introducao.py
@@ -1,1 +1,13 @@
-print('Hello World')
+#matplotlib
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+t = np.linspace(0, np.pi, 500)
+y = np.cos(4*t)
+
+plt.plot(t, y)
+plt.title('Gráfico do cosseno')
+plt.xlabel('Tempo (s)')
+plt.ylabel('Amplitude')
+plt.savefig('grafico_cos.png') 
