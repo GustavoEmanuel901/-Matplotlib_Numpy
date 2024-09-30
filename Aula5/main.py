6d1a53784c5b682949b03c027cc4ad78be04c19b
Gustavo Emanuel <62678551+GustavoEmanuel901@users.noreply.github.com>
2024-09-30 00:59:01 +0000

    Aula5

---
+++ Aula5/main.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(0, 5, 0.1)
+y1 = x**5
+y2 = x**2
+
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(nrows=2, ncols=2, figsize=(8, 4))
+ax1.plot(x, y1)
+ax2.plot(x, y2)
+ax3.plot(x, y1)
+ax4.plot(x, y2)
+
+plt.savefig('gráfico_exponencial.png')
